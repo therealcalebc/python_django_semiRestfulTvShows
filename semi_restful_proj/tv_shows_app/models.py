e109a64a9519441fc8569c79e249215dbd8145a6
Caleb A Comstock <c_comstock@rocketmail.com>
2021-02-24 01:49:24 -0800

    added server side form data validation

--- semi_restful_proj/tv_shows_app/models.py
+++ semi_restful_proj/tv_shows_app/models.py
@@ -1,4 +1,23 @@
 from django.db import models
+from datetime import datetime, date
+
+class ShowManager(models.Manager):
+	def basic_validator(self, postData):
+		errors = {}
+		if len(postData['title']) < 2:
+			errors["title"] = "Title must be at least 2 characters"
+		if len(postData['network']) < 3:
+			errors['network'] = "Network must be at least 3 characters"
+		try:
+			if datetime.strptime(postData['release_date'], "%Y-%m-%d").date() < date(1928, 7, 2):
+				errors['release_date'] = "Release date must be after July 1st, 1928"
+			elif datetime.strptime(postData['release_date'], "%Y-%m-%d").date() > date.today():
+				errors['release_date'] = "Release date must be in the past"
+		except:
+			errors['release_date'] = "Enter a valid date (ex: 07/02/1928)"
+		if postData['description'] and len(postData['description']) < 10:
+			errors['description'] = "Description is optional but must be at least 10 characters if included"
+		return errors
 
 class Show(models.Model):
 	# id = models.IntegerField() - primary key
@@ -8,3 +27,4 @@ class Show(models.Model):
 	description = models.TextField(blank=True, default='')
 	created_at = models.DateTimeField(auto_now_add=True)
 	updated_at = models.DateTimeField(auto_now=True)
+	objects = ShowManager()
--- semi_restful_proj/tv_shows_app/views.py
+++ semi_restful_proj/tv_shows_app/views.py
@@ -1,56 +1,88 @@
 from django.shortcuts import render, redirect, HttpResponse
+from django.contrib import messages
+#from django.contrib.messages import constants as messages
 from datetime import datetime, date
 from .models import Show
+
+# MESSAGE_TAGS = {
+# 	messages.ERROR: 'danger'
+# }
 
 title = 'Semi-RESTful TV Shows'
 networks = ('ABC', 'CBS', 'FOX', 'NBC')
 
+#=============================
+# -- Processing/Redirects --
+#=============================
 def index(request):
 	return redirect('/shows')
 
-def view_shows_list(request):
-	shows = Show.objects.all()
-	return render(request, 'index.html', { 'title': title, 'shows': shows })
-
-def new_show(request):
-	return render(request, 'create.html', { 'title': title, 'networks': networks })
-
 def create_show(request):
+	errors = Show.objects.basic_validator(request.POST)
+	if errors:
+		for key, value in errors.items():
+			messages.error(request, value, extra_tags="alert-danger")
+		request.session['repopnew'] = {
+			'title': request.POST['title'],
+			'network': request.POST['network'],
+			'release_date': request.POST['release_date'],
+			'description': request.POST['description']
+		}
+		return redirect("/shows/new")
+	if Show.objects.filter(title=request.POST['title']):
+		messages.error(request, "Title must be unique", extra_tags="alert-danger")
+		request.session['repopnew'] = {
+			'title': request.POST['title'],
+			'network': request.POST['network'],
+			'release_date': request.POST['release_date'],
+			'description': request.POST['description']
+		}
+		return redirect("/shows/new")
 	s = Show.objects.create(title=request.POST['title'], network=request.POST['network'], release_date=datetime.strptime(request.POST['release_date'], "%Y-%m-%d").date(), description=request.POST['description'])
+	messages.success(request, "New show successfully created", extra_tags="alert-success")
 	return redirect(f"/shows/{s.id}")
 
-def view_show(request, id):
-	s = Show.objects.get(id=id)
-	return render(request, 'read.html', { 'title': title, 'show': s })
-
-def edit_show(request, id):
-	s = Show.objects.get(id=id)
-	return render(request, 'update.html', { 'title': title, 'show': s, 'networks': networks })
-
 def update_show(request, id):
+	errors = Show.objects.basic_validator(request.POST)
+	if errors:
+		for key, value in errors.items():
+			messages.error(request, value, extra_tags="alert-danger")
+		return redirect(f"/shows/{id}/edit")
 	s = Show.objects.get(id=id)
-	title = request.POST['title']
-	network = request.POST['network']
-	release_date = datetime.strptime(request.POST['release_date'], "%Y-%m-%d").date()
-	description = request.POST['description']
-	save_required = False
-	if title and title != s.title:
-		s.title = title
-		save_required = True
-	if network and network != s.network:
-		s.network = network
-		save_required = True
-	if release_date and release_date != s.release_date:
-		s.release_date = release_date
-		save_required = True
-	if description != s.description:
-		s.description = description
-		save_required = True
-	if save_required:
-		s.save()
+	s.title = request.POST['title']
+	s.network = request.POST['network']
+	s.release_date = datetime.strptime(request.POST['release_date'], "%Y-%m-%d").date()
+	s.description = request.POST['description']
+	s.save()
+	messages.success(request, "Show info successfully updated", extra_tags="alert-success")
 	return redirect(f"/shows/{s.id}")
 
 def delete_show(request, id):
 	s = Show.objects.get(id=id)
 	s.delete()
 	return redirect("/shows")
+
+#=============================
+# -- Rendering --
+#=============================
+def new_show(request):
+	context = { 'title': title, 'default_networks': networks }
+	if 'repopnew' in request.session:
+		context['repopnew'] = request.session['repopnew']
+		del request.session['repopnew']
+	return render(request, 'create.html', context)
+
+
+def view_shows_list(request):
+	shows = Show.objects.all()
+	return render(request, 'index.html', {'title': title, 'shows': shows})
+
+
+def view_show(request, id):
+	s = Show.objects.get(id=id)
+	return render(request, 'read.html', {'title': title, 'show': s})
+
+
+def edit_show(request, id):
+	s = Show.objects.get(id=id)
+	return render(request, 'update.html', {'title': title, 'show': s, 'default_networks': networks})
